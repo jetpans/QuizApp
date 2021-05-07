f3f5edb802474300216fb4168cf9a5ef105b544f
Stjepan Đelekovčan <45045855+jetpans@users.noreply.github.com>
2021-05-07 12:16:32 +0200

    Add files via upload

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,12 +1,32 @@
-import React from "react";
-import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 export default function InputBox(props) {
   return (
     <View style={styles.inputFrame}>
-      <Text style={{ fontSize: 20, fontFamily: "Helvetica" }}>
-        {props.title}
-      </Text>
+      <View style={styles.upperFrame}>
+        <View style={styles.textFrame}>
+          <Text style={{ fontSize: 20, fontFamily: "Helvetica" }}>
+            {props.title}
+          </Text>
+        </View>
+        <View style={styles.buttonFrame}>
+          {props.title != "Question:" ? (
+            <TouchableOpacity
+              style={{
+                width: 30,
+                height: 30,
+                borderRadius: 25,
+                borderWidth: 1,
+                backgroundColor: props.color,
+              }}
+              onPress={props.chosen}
+            >
+              <View></View>
+            </TouchableOpacity>
+          ) : null}
+        </View>
+      </View>
       <TextInput
         style={styles.textBox}
         value={props.question}
@@ -32,5 +52,29 @@ const styles = {
     paddingRight: 2,
     fontSize: 14,
     outline: "None",
+  },
+  circle: {
+    width: "8%",
+    height: "8%",
+    borderRadius: 50,
+  },
+  upperFrame: {
+    height: "50%",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonFrame: {
+    height: "100%",
+    width: "30%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textFrame: {
+    height: "100%",
+    width: "60%",
+    paddingLeft: "5%",
+    justifyContent: "center",
+    alignItems: "flex-start",
   },
 };
--- pages/QuestionForm.js
+++ pages/QuestionForm.js
@@ -1,6 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { View, Text, TextInput } from "react-native";
 import InputBox from "../components/InputBox";
+import OptionButton from "../components/OptionButton";
+const buttonReducer = (buttons, action) => {
+  switch (action.type) {
+    case "updateButtons":
+      console.log("I JA SAM U REDUCERU");
+      let temp = ["white", "white", "white", "white"];
+      temp[action.chosen] = "green";
+      console.log(temp);
+      return temp;
+  }
+};
 
 const QuestionForm = (props) => {
   const [question, setQuestion] = useState("");
@@ -8,7 +19,16 @@ const QuestionForm = (props) => {
   const [answer2, setAnswer2] = useState("");
   const [answer3, setAnswer3] = useState("");
   const [answer4, setAnswer4] = useState("");
+  const [buttons, dispatchButtons] = useReducer(buttonReducer, [
+    "white",
+    "white",
+    "white",
+    "white",
+  ]);
 
+  const handleClickOn = (input) => {
+    dispatchButtons({ type: "updateButtons", chosen: input });
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -30,30 +50,60 @@ const QuestionForm = (props) => {
         function={setQuestion}
         placeholder={"Your question here..."}
       ></InputBox>
+
       <InputBox
         quesiton={answer1}
         title="First Answer:"
         function={setAnswer1}
         placeholder={"Your answer here..."}
+        chosen={() => {
+          handleClickOn(0);
+        }}
+        color={buttons[0]}
       ></InputBox>
+
       <InputBox
         quesiton={answer2}
         title="Second Answer:"
         function={setAnswer2}
         placeholder={"Your answer here..."}
+        chosen={() => {
+          handleClickOn(1);
+        }}
+        color={buttons[1]}
       ></InputBox>
+
       <InputBox
         quesiton={answer3}
         title="Third Answer:"
         function={setAnswer3}
         placeholder={"Your answer here..."}
+        chosen={() => {
+          handleClickOn(2);
+        }}
+        color={buttons[2]}
       ></InputBox>
+
       <InputBox
         quesiton={answer4}
         title="Fourth Answer:"
         function={setAnswer4}
         placeholder={"Your answer here..."}
+        chosen={() => {
+          handleClickOn(3);
+        }}
+        color={buttons[3]}
       ></InputBox>
+
+      <View style={styles.goback}>
+        <OptionButton
+          answer="MAIN MENU"
+          color="green"
+          function={() => {
+            props.navigation.navigate("Main");
+          }}
+        ></OptionButton>
+      </View>
     </View>
   );
 };
@@ -61,12 +111,23 @@ const QuestionForm = (props) => {
 export default QuestionForm;
 
 const styles = {
-  container: { backgroundColor: "#f5f5f5", height: "100%", width: "100%" },
+  container: {
+    backgroundColor: "#f5f5f5",
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+  },
   header: {
     backgroundColor: "#6ad2f7",
     height: "20%",
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+  goback: {
+    width: "100%",
+    height: "10%",
+    justifyContent: "center",
+    alignContent: "center",
   },
 };
